158309361d5e44f5ba35e0433bae59ff2381f661
janpol <148458914+barrozojp@users.noreply.github.com>
2024-02-12 20:07:07 +0800

    FIXED THE VALIDATION for REGISTRATION (John Paul)

--- app/src/main/java/com/codeofduty/appointcare/Registration.kt
+++ app/src/main/java/com/codeofduty/appointcare/Registration.kt
@@ -1,15 +1,17 @@
 package com.codeofduty.appointcare
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
-import com.codeofduty.appointcare.databinding.ActivityRegistrationBinding
+import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputLayout
 import com.jakewharton.rxbinding2.widget.RxTextView
+import com.codeofduty.appointcare.databinding.ActivityRegistrationBinding
 import io.reactivex.Observable
 
 
@@ -22,108 +24,6 @@ class Registration : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        //First Name Validation//
-
-        val FNameStream = RxTextView.textChanges(binding.FNameRegEditText)
-            .skipInitialValue()
-            .map { name ->
-                name.isEmpty()
-            }
-        FNameStream.subscribe{
-            showFNameExistAlert(it)
-        }
-        //Last Name Validation//
-
-        val LNameStream = RxTextView.textChanges(binding.LNameRegEditText)
-            .skipInitialValue()
-            .map { name ->
-                name.isEmpty()
-            }
-        LNameStream.subscribe{
-            showLNameExistAlert(it)
-        }
-
-        //Phone Number Validation
-
-        val phoneNumberStream = RxTextView.textChanges(binding.PhoneNumRegEditText)
-            .skipInitialValue()
-            .map { phoneNumber ->
-                phoneNumber.length >= 10
-            }
-        phoneNumberStream.subscribe{
-            showPhoneNumExistAlert(it)
-        }
-
-        //EMAIL VALIDATION
-
-        val EMAILStream = RxTextView.textChanges(binding.emailRegEditText)
-            .skipInitialValue()
-            .map { email ->
-                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
-            }
-        EMAILStream.subscribe{
-            showEMAILExistAlert(it)
-        }
-        //AGE Validation//
-
-        val AgeStream = RxTextView.textChanges(binding.AgeRegEditText)
-            .skipInitialValue()
-            .map { age ->
-                age.isEmpty()
-            }
-        AgeStream.subscribe{
-            showAgeExistAlert(it)
-        }
-
-        //PASSWORD VALIDATION
-        val passwordStream = RxTextView.textChanges(binding.PasswordRegEditText)
-            .skipInitialValue()
-            .map { password ->
-                password.length <8
-            }
-        passwordStream.subscribe{
-            showPasswordAlert(it)
-        }
-        // CONFIRM PASSWORD VALIDATION
-        val confirmPasswordStream = Observable.merge(
-            RxTextView.textChanges(binding.PasswordRegEditText)
-                .skipInitialValue()
-                .map { password ->
-                    password.toString() !=binding.ConfirmPasswordRegEditText.text.toString()
-                },
-            RxTextView.textChanges(binding.ConfirmPasswordRegEditText)
-                .skipInitialValue()
-                .map { confirmPassword ->
-                    confirmPassword.toString() != binding.PasswordRegEditText.text.toString()
-                })
-        confirmPasswordStream.subscribe {
-            showConfirmPasswordAlert(it)
-        }
-
-        // Register AS Validation
-        val registerAsStream = Observable.create<Boolean> { emitter ->
-            binding.registerAS.setOnCheckedChangeListener { group, checkedId ->
-                val isValid = checkedId != -1
-                emitter.onNext(isValid)
-            }
-        }
-        registerAsStream.subscribe { isValid ->
-            showRegisterAsAlert(!isValid)
-        }
-
-        // Gender Validation
-        val genderStream = Observable.create<Boolean> { emitter ->
-            binding.gender.setOnCheckedChangeListener { group, checkedId ->
-                val isValid = checkedId != -1
-                emitter.onNext(isValid)
-            }
-        }
-        genderStream.subscribe { isValid ->
-            showGenderAlert(!isValid)
-        }
-
-
 
         // RADIO GROUP//
         val radioGroup = findViewById<RadioGroup>(R.id.gender)
@@ -164,6 +64,138 @@ class Registration : AppCompatActivity() {
                 doctorRadioButton.isChecked = false
             }
             showToast("Patient")
+        }
+
+        // Register AS Validation
+        val registerAsStream = Observable.create<Boolean> { emitter ->
+            binding.registerAS.setOnCheckedChangeListener { group, checkedId ->
+                val isValid = checkedId != -1
+                emitter.onNext(isValid)
+            }
+        }
+        registerAsStream.subscribe { isValid ->
+            showRegisterAsAlert(!isValid)
+        }
+
+        //First Name Validation//
+
+        val FNameStream = RxTextView.textChanges(binding.FNameRegEditText)
+            .skipInitialValue()
+            .map { name ->
+                name.isEmpty()
+            }
+        FNameStream.subscribe{
+            showFNameExistAlert(it)
+        }
+        //Last Name Validation//
+
+        val LNameStream = RxTextView.textChanges(binding.LNameRegEditText)
+            .skipInitialValue()
+            .map { name ->
+                name.isEmpty()
+            }
+        LNameStream.subscribe{
+            showLNameExistAlert(it)
+        }
+
+        //Phone Number Validation
+
+        val phoneNumberStream = RxTextView.textChanges(binding.PhoneNumRegEditText)
+            .skipInitialValue()
+            .map { phoneNumber ->
+                phoneNumber.length >= 11
+            }
+        phoneNumberStream.subscribe{
+            showPhoneNumExistAlert(it)
+        }
+        // Gender Validation
+        val genderStream = Observable.create<Boolean> { emitter ->
+            binding.gender.setOnCheckedChangeListener { group, checkedId ->
+                val isValid = checkedId != -1
+                emitter.onNext(isValid)
+            }
+        }
+        genderStream.subscribe { isValid ->
+            showGenderAlert(!isValid)
+        }
+
+
+        //EMAIL VALIDATION
+
+        val EMAILStream = RxTextView.textChanges(binding.emailRegEditText)
+            .skipInitialValue()
+            .map { email ->
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            }
+        EMAILStream.subscribe{
+            showEMAILExistAlert(it)
+        }
+        //AGE Validation//
+
+        val AgeStream = RxTextView.textChanges(binding.AgeRegEditText)
+            .skipInitialValue()
+            .map { age ->
+                age.isEmpty()
+            }
+        AgeStream.subscribe{
+            showAgeExistAlert(it)
+        }
+
+        //PASSWORD VALIDATION
+        val passwordStream = RxTextView.textChanges(binding.PasswordRegEditText)
+            .skipInitialValue()
+            .map { password ->
+                password.length <8
+            }
+        passwordStream.subscribe{
+            showPasswordAlert(it)
+        }
+        // CONFIRM PASSWORD VALIDATION
+        val confirmPasswordStream = io.reactivex.Observable.merge(
+            RxTextView.textChanges(binding.PasswordRegEditText)
+                .skipInitialValue()
+                .map { password ->
+                    password.toString() !=binding.ConfirmPasswordRegEditText.text.toString()
+                },
+            RxTextView.textChanges(binding.ConfirmPasswordRegEditText)
+                .skipInitialValue()
+                .map { confirmPassword ->
+                    confirmPassword.toString() != binding.PasswordRegEditText.text.toString()
+                })
+        confirmPasswordStream.subscribe {
+            showConfirmPasswordAlert(it)
+        }
+
+        //ENABLE BUTTON TRUE OR FALSE
+
+        val invalidFieldStream = io.reactivex.Observable.combineLatest(
+            FNameStream,
+            LNameStream,
+            phoneNumberStream,
+            EMAILStream,
+            AgeStream,
+            passwordStream,
+            confirmPasswordStream,
+            { FnameInvalid: Boolean, LnameInvalid: Boolean, phonenumInvalid: Boolean, emailInvalid: Boolean, ageInvalid: Boolean, passwordInvalid: Boolean, confirmPassInvalid: Boolean ->
+                !FnameInvalid && !LnameInvalid && !phonenumInvalid && !emailInvalid &&! ageInvalid && !passwordInvalid && !confirmPassInvalid
+            })
+        invalidFieldStream.subscribe { isValid ->
+            if (isValid) {
+                binding.btnRegister.isEnabled = true
+                binding.btnRegister.backgroundTintList = ContextCompat.getColorStateList(this, R.color.primarycolor)
+            } else {
+                binding.btnRegister.isEnabled = false
+                binding.btnRegister.backgroundTintList = ContextCompat.getColorStateList(this, android.R.color.darker_gray)
+            }
+        }
+
+
+// CLICK
+        binding.btnRegister.setOnClickListener{
+            startActivity(Intent(this, LogIn::class.java))
+        }
+        binding.haveAccBTN.setOnClickListener{
+            startActivity(Intent(this, LogIn::class.java))
         }
 
     }
@@ -233,6 +265,4 @@ class Registration : AppCompatActivity() {
             Toast.makeText(this, "Please select your gender", Toast.LENGTH_SHORT).show()
         }
     }
-
-
 }
